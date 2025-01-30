502ef87bf217ba0c76432b82fd83d37554cd5db0
innodwin <innocentedwin25@yahoo.com>
2025-01-30 23:33:04 +0100

    enforced jwt swagger endpoint

--- system/src/main/java/com/Hospital_mang/system/config/SwaggerConfig.java
+++ system/src/main/java/com/Hospital_mang/system/config/SwaggerConfig.java
@@ -8,16 +8,30 @@ package com.Hospital_mang.system.config;
 import java.util.Arrays;
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.*;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 /**
  *
  * @author Idoko Date: Dec 7, 2023
  */
 @Configuration
+@OpenAPIDefinition(
+        security = @SecurityRequirement(name = "Authorization") // Use "Authorization" as the header
+)
+@SecurityScheme(
+        name = "Authorization",  // Header must be named "Authorization"
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT"
+)
 public class SwaggerConfig {
     @Bean
     public GroupedOpenApi publicApi() {
