1749a4ba30eef0e2d3e82acca23fdaba9f989b7d
innodwin <innocentedwin25@yahoo.com>
2025-01-30 09:04:43 +0100

    modified jwt flow

--- system/src/main/java/com/Hospital_mang/system/Controller/AuthenticationController.java
+++ system/src/main/java/com/Hospital_mang/system/Controller/AuthenticationController.java
@@ -10,9 +10,9 @@ import com.Hospital_mang.system.Service.UserService;
 import com.Hospital_mang.system.request.JwtRequest;
 import com.Hospital_mang.system.request.RefreshTokenRequest;
 import com.Hospital_mang.system.response.JwtResponse;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -27,8 +27,7 @@ import org.springframework.web.bind.annotation.*;
 //@CrossOrigin
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
-@Api(value = "", tags = {"Authentication Service"})
-@Tag(name = "Authentication Service", description = "This class handles all authentication within users account, including login")
+@Tag(name = "Authentication Controller", description = "Handles operations related to login, user management and security")
 public class AuthenticationController {
 
 
@@ -38,15 +37,24 @@ public class AuthenticationController {
     @Autowired
     private AuthService authService;
 
-    @ApiOperation(value = "login user authentication.")
-    @ApiResponse(code = 200, message = "created")
+    @Operation(summary = "Authentication call. Users are to provide their usernames and passwords to enable this operation")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User  validated"),
+            @ApiResponse(responseCode = "404", description = "Username or password not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PostMapping({"/login"})
     public JwtResponse Login(@Valid @RequestBody JwtRequest jwtRequest) {
         return authService.LoginUser(jwtRequest);
     }
 
+    @Operation(summary = "Refresh Token. This helps to refresh a jwt token from expiring")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful"),
+            @ApiResponse(responseCode = "404", description = "Failed")
+    })
     @PostMapping("/refreshtoken")
-    @ApiOperation(value = "This method handles refresh token", response = String.class)
+    //@ApiOperation(value = "This method handles refresh token", response = String.class)
     public ResponseEntity<?> refreshtoken(@Valid @RequestBody RefreshTokenRequest request) {
         return authService.refreshToken(request);
     }
